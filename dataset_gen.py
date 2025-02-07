Fixed data domains + Added pre computation of cost and time

dataset_gen.py
@@ -7,21 +7,21 @@ class Dataset:
         if values_domains is None:
             values_domains = {
                 "cloud_machine": {
-                    "cpu_rate": [3000, 5000],  # MIPS
-                    "cpu_usage_cost": [0.7, 1.0],  # €/h
-                    "ram_usage_cost": [0.02, 0.05],  # €/GB/h
+                    "cpu_rate": [3000, 5000],  # MIPS (1e6)
+                    "cpu_usage_cost": [0.7/3600, 1.0/3600],  # €/s
+                    "ram_usage_cost": [0.02/3600, 0.05/3600],  # €/GB/s
                     "bandwidth_usage_cost": [0.05, 0.1],  # €/GB
-                    "latency": [200, 300],  # ms
+                    "latency": [0.01, 3],  # s
                 },
                 "fog_machine": {
-                    "cpu_rate": [500, 1500],  # MIPS
-                    "cpu_usage_cost": [0.1, 0.4],  # €/h
-                    "ram_usage_cost": [0.01, 0.03],  # €/GB/h
-                    "bandwidth_usage_cost": [0.01, 0.2],  # €/GB
-                    "latency": [10, 50],  # ms
+                    "cpu_rate": [500, 1500],  # MIPS (1e6)
+                    "cpu_usage_cost": [0.1/3600, 0.4/3600],  # €/s
+                    "ram_usage_cost": [0.01/3600, 0.03/3600],  # €/GB/s
+                    "bandwidth_usage_cost": [0.01, 0.02],  # €/GB
+                    "latency": [0.005, 0.01],  # s
                 },
                 "task": {
-                    "num_instructions": [1, 1],  # 1 billion (1e9) instructions
+                    "num_instructions": [1000, 1000],  # millions (1e6) of instructions
                     "ram_required": [0.05, 0.2],  # GB
                     "in_traffic": [0.01, 0.1],  # GB
                     "out_traffic": [0.01, 0.1],  # GB
@@ -34,6 +34,33 @@ class Dataset:
         fog_machines = self.gen_machines(num_fog_machines, values_domains["fog_machine"])
         self._machines = np.concatenate((cloud_machines, fog_machines))
         self._tasks = self.gen_tasks(num_tasks, values_domains["task"])
+
+        # pre-compute objective values
+        self._run_time_matrix, self._cost_matrix = self._compute_run_time_cost_matrices()
+
+    @staticmethod
+    def _compute_execution_time(task, machine):
+        return task["num_instructions"] / machine["cpu_rate"]
+
+    @staticmethod
+    def _compute_cost(task, machine, exec_time):
+        return (machine["cpu_usage_cost"] + machine["ram_usage_cost"] * task["ram_required"]) * exec_time
+
+    @staticmethod
+    def _compute_total_time(machine, exec_time):
+        return exec_time + machine["latency"]
+
+    def _compute_run_time_cost_matrices(self):
+        run_time_matrix = np.zeros((self.get_num_tasks(), self.get_num_machines()))
+        cost_matrix = np.zeros((self.get_num_tasks(), self.get_num_machines()))
+
+        for i, task in enumerate(self.get_tasks()):
+            for j, machine in enumerate(self.get_machines()):
+                exec_time = self._compute_execution_time(task, machine)  # used for the 2 objectives
+                cost_matrix[i, j] = self._compute_cost(task, machine, exec_time)
+                run_time_matrix[i, j] = self._compute_total_time(machine, exec_time)
+
+        return run_time_matrix, cost_matrix
 
     def gen_machines(self, num_machines, variables_domain):
         machines = np.empty(num_machines, dtype=object)
@@ -82,6 +109,12 @@ class Dataset:
     def get_num_machines(self):
         return len(self._machines)
 
+    def get_cost(self, task_idx, machine_idx):
+        return self._cost_matrix[task_idx, machine_idx]
+
+    def get_run_time(self, task_idx, machine_idx):
+        return self._run_time_matrix[task_idx, machine_idx]
+
 
 # TEST
 
@@ -89,7 +122,12 @@ dataset = Dataset(num_cloud_machines=3, num_fog_machines=10, num_tasks=10)
 
 pop = dataset.init_pop(pop_size=3)
 print(pop)
-print(dataset.get_machines())
-print(dataset.get_tasks())
+
+print("task 0:", dataset.get_tasks()[0])
+print("machine 0:", dataset.get_machines()[0])
+
+print(f"Cost of task 0 on machine 0: {dataset.get_cost(0, 0)}")
+print(f"Time of task 0 on machine 0: {dataset.get_run_time(0, 0)}")
+
 
 